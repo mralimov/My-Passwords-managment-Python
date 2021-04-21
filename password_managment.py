62f65d8ec80d4cba823c623744349dd3e656bee4
mralimov <mralimov89@gmail.com>
2021-04-21 11:24:28 -0400

    Set up UI

--- password_managment.py
+++ password_managment.py
@@ -2,3 +2,17 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, shuffle, choice
 import pyperclip
+from password_create import password_generator
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("My Password manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+my_pass_img = PhotoImage(file="./logo.png")
+canvas.create_image(100, 100, image=my_pass_img)
+canvas.grid(row=0, column=1)

d68b4e74e575fda3f7c13dcaa0005d132a0b6453
mralimov <mralimov89@gmail.com>
2021-04-21 11:24:53 -0400

    Setup UI labels

--- password_managment.py
+++ password_managment.py
@@ -16,3 +16,11 @@ canvas = Canvas(width=200, height=200)
 my_pass_img = PhotoImage(file="./logo.png")
 canvas.create_image(100, 100, image=my_pass_img)
 canvas.grid(row=0, column=1)
+
+
+website_label = Label(text="Website: ")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
